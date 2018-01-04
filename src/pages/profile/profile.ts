27d54cd069b2ba307e7bb13a78ce1f0b15391282
daniel aguiar <bsb.aguiar@gmail.com>
2018-01-03 22:57:01 -0200

    tratamento de erros na aplicação

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,40 +1,57 @@
-import { ClienteDTO } from './../../models/cliente.dto';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ClienteDTO } from "./../../models/cliente.dto";
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
-import { StorageService } from '../../services/storage.service';
-import { ClienteService } from './../../services/domain/cliente.service';
-import { API_CONFIG } from '../../config/api.config';
-
+import { StorageService } from "../../services/storage.service";
+import { ClienteService } from "./../../services/domain/cliente.service";
+import { API_CONFIG } from "../../config/api.config";
+import { AppAbstractBaseComponent } from "../../app/app-abstract-base.component";
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+  selector: "page-profile",
+  templateUrl: "profile.html"
 })
-export class ProfilePage {
-
+export class ProfilePage extends AppAbstractBaseComponent {
   cliente: ClienteDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage : StorageService, public clienteService: ClienteService ) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService
+  ) {
+    super();
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     console.log(localUser.email);
-    if (localUser && localUser.email){
-        this.clienteService.findByEmail(localUser.email)
-        .subscribe(response => {
-            console.log(response);
-            this.cliente = response;
-            this.getImageIfExists();
-
-        }, error=> {} )
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email).subscribe(
+        response => {
+          console.log(response);
+          this.cliente = response;
+          this.getImageIfExists();
+        },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot(this.paginas.home);
+          }
+        }
+      );
     }
   }
 
   private getImageIfExists() {
-    this.clienteService.getImageFromBucket(this.cliente.id)
-    .subscribe(response => this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`, error=>{});
+    this.clienteService
+      .getImageFromBucket(this.cliente.id)
+      .subscribe(
+        response =>
+          (this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${
+            this.cliente.id
+          }.jpg`),
+        error => {}
+      );
   }
 }
